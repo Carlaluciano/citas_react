27f8862bd1fb3ef19accc04278b8da86e078f0b6
unknown <carla.luciano@hotmail.com>
2022-12-01 14:37:12 -0300

    citas_vite

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import Headers from "./Components/Header"
+import Formulario from "./Components/Formulario"
+import ListadoPacientes from "./Components/ListadoPacientes"
+import { useState, useEffect } from "react"
+
+function App() {
+
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({}); 
+
+  // LocalStorage
+
+ useEffect(() => {
+ const obtenerLS = () => {
+  const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+setPacientes(pacientesLS)
+}
+obtenerLS();
+}, []);
+
+  useEffect(() => {
+  localStorage.setItem('paciente', JSON.stringify(pacientes))
+  },[pacientes]);
+
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id)
+    setPacientes(pacientesActualizados)
+  }
+  
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Headers/>
+      <div className="mt-12 md:flex">
+      <Formulario
+      pacientes={pacientes}
+      setPacientes={setPacientes}
+      paciente={paciente}
+      setPaciente={setPaciente}
+      />
+      <ListadoPacientes
+      pacientes={pacientes}
+      setPaciente={setPaciente}
+      eliminarPaciente={eliminarPaciente}
+      
+      />
+      </div>
+    </div>
+  )
+}
+
+export default App
